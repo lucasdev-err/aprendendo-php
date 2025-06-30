df3cdd03b155c9df51bacd1a42e874aa12fa2bd5
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-06-30 20:39:20 -0300

    :white_check_mark: Exercicio 002 Concluido

--- alura/exercicios/ex002.php
+++ alura/exercicios/ex002.php
@@ -1,1 +1,14 @@
 2 - Crie um programa em PHP que calcule a média de três notas e exiba o resultado.
+
+<?php 
+
+
+$somanotas = $argv[1];
+$somanotas += $argv[2];
+$somanotas += $argv[3];
+
+$media = $somanotas / 3;
+
+echo "A media entre $argv[1], $argv[2] e $argv[3] e: $media";
+
+?>
