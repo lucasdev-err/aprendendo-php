0881159758afbf10c030d2aac1ef3821da05ada9
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-06-30 20:42:25 -0300

    :white_check_mark: Exercicio 003 concluido.

--- alura/exercicios/ex003.php
+++ alura/exercicios/ex003.php
@@ -1,1 +1,10 @@
 3 - Elabore um programa em PHP que receba um valor em metros e converta para centímetros.
+
+<?php 
+
+$metros = $argv[1];
+$centimetros = $metros*100;
+
+echo "$metros metros, sao $centimetros centimetros."
+
+?>
