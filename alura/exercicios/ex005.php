5e9fadd4408742c0cd71893391445cbe6c47f08d
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-06-30 20:56:22 -0300

    :white_check_mark: Exercicio 005 concluido.

--- alura/exercicios/ex005.php
+++ alura/exercicios/ex005.php
@@ -1,1 +1,11 @@
 5 - Escreva um programa em PHP que converta uma temperatura de Celsius para Fahrenheit.
+
+
+<?php 
+
+$celsius = $argv[1];
+$fahrenheit = ($celsius * (9/5)) + 32;
+
+echo "$celsius graus celsius sao $fahrenheit graus fahrenheit.";
+
+?>
