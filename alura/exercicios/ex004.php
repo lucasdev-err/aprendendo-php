1787a05648e268ab6981fe8bf628198ba2045d2d
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-06-30 20:50:55 -0300

    :white_check_mark: Exercicio 004 concluido.

--- alura/exercicios/ex004.php
+++ alura/exercicios/ex004.php
@@ -1,1 +1,13 @@
 4 - Desenvolva um programa em PHP que verifique se um ano é bissexto ou não.
+
+<?php 
+
+$ano = $argv[1];
+
+if ($ano%4 == 0 && $ano%100 != 0|| $ano%400 == 0){
+    echo "Bisexto";
+} else{
+    echo "normal";
+}
+
+?>
