07a4f49ea55067a9558505bdbb1c0c56d58e7b20
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-07 18:50:57 -0300

    :white_check_mark: Exercicio 008 concluido

--- alura/exercicios/ex008.php
+++ alura/exercicios/ex008.php
@@ -1,1 +1,20 @@
 <!-- Desenvolva um programa que exiba na tela uma saudação (bom dia, boa tarde ou boa noite) dependendo do horário encontrado em uma variável (inteiro representando as horas). -->
+
+
+<?php 
+date_default_timezone_set("America/Sao_paulo");
+$hora = date("H");
+
+echo "$hora horas\n";
+
+if ($hora >= 6 && $hora < 12){
+    echo "Bom dia!";
+} elseif ($hora >= 12 && $hora < 18) {
+    echo "Boa tarde!";
+} elseif ($hora >= 18 && $hora < 24) {
+    echo "Boa noite!";
+} else {
+    echo "Boa madrugada";
+}
+
+?>
