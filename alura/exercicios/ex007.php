e278c9c763b227e60f45b3fdd519252d4e227062
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-07 18:37:55 -0300

    :white_check_mark: Exercicio 007 concluido

--- alura/exercicios/ex007.php
+++ alura/exercicios/ex007.php
@@ -1,2 +1,26 @@
 <!-- Crie um programa que, a partir de altura e peso, calcule o IMC e exiba a classificação do IMC. -->
 
+<?php 
+
+$peso = $argv[1];
+$altura = $argv[2];
+
+$imc = $peso / ($altura**2);
+
+if ($imc < 18.5){
+    echo "Abaixo do peso\n";
+} elseif ($imc < 24.9) {
+    echo "Peso normal\n";
+} elseif ($imc < 29.9) {
+    echo "Sobrepeso\n";
+} elseif ($imc < 34.9) {
+    echo "Obesidade I\n";
+} elseif ($imc < 39.9) {
+    echo "Obesidade II\n";
+} else {
+    echo "Obesidade III\n";
+}
+
+echo number_format($imc, 2, ",", ".") . " Kg/m²";
+
+?>
