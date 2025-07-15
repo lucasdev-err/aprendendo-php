960f6f5299d04a266ce1131ac7c4318b4505ba1e
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-14 21:22:22 -0300

    :white_check_mark: Exercicio 02/003 concluido

--- alura/exercicios/02/ex003.php
+++ alura/exercicios/02/ex003.php
@@ -2,4 +2,10 @@
 
 // Crie uma função em PHP que converta graus celsius para Fahrenheit.
 
+function celciusToFahrenheit(float $celsius,): float {
+    return ($celsius * (9/5)) + 32;
+}
+
+echo number_format(celciusToFahrenheit(22), 1);
+
 ?>
