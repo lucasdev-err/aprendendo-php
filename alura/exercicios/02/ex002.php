4f7fa72449215987bb74d5243524f0be3533a778
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-14 21:18:05 -0300

    :white_check_mark: Exercicio 02/002 concluido

--- alura/exercicios/02/ex002.php
+++ alura/exercicios/02/ex002.php
@@ -2,4 +2,10 @@
 
 // Crie uma função em PHP que calcule o IMC baseado na altura e peso passados por parâmetro.
 
+function imc(float $peso, float $altura): float {
+    return $peso / ($altura**2);
+}
+
+echo number_format(imc(76, 1.85), 2);
+
 ?>
