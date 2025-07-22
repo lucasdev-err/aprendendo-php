55159f4b7e31c7532ec09fdaab265f3fec052452
Lucas <lucas.dev.err@gmail.com>
2025-07-22 11:52:13 +0000

    :white_check_mark: Execicio 02/004 concluido

--- alura/exercicios/02/ex004.php
+++ alura/exercicios/02/ex004.php
@@ -2,6 +2,10 @@
 
 // Escreva um programa em PHP que inicialize um array de notas e exiba somente as 3 maiores notas do array.
 
+$notas = [10, 2, 5, 3, 2, 8, 10];
 
+rsort($notas);
+
+echo "As 3 maiores notas são: $notas[0], $notas[1] e $notas[2]\n";
 
 ?>
