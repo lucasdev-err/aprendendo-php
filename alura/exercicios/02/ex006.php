c801f0ad0be486b8b7580ed8f098338fddd1bfac
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-22 18:26:50 -0300

    :white_check_mark: Exercicio 02/006 concluido

--- alura/exercicios/02/ex006.php
+++ alura/exercicios/02/ex006.php
@@ -1,6 +1,20 @@
 <?php
 
 // Escreva uma funçao em PHP que receba um array de strings por parâmetro e o retorne ordenado em ordem alfabética.
+function alfabetica(array $arr): array {
+    sort($arr);
+    return($arr);
+}
 
+$array = [
+    "Bola",
+    "Seila",
+    "Andre",
+    "Lucas"
+];
+
+$arrayOrdenado = alfabetica($array);
+
+var_dump($arrayOrdenado);
 
 ?>
