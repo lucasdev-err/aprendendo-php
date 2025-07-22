0b414c49cbdacd3ed40aee38660c556e60c51aca
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-22 18:20:28 -0300

    :white_check_mark: Exercicio 02/005 concluido

--- alura/exercicios/02/ex005.php
+++ alura/exercicios/02/ex005.php
@@ -2,5 +2,9 @@
 
 //Crie um programa em PHP que transforme a string "Vinicius Dias, 1997, Programador" em um array em que cada item esta separa por virgulas.
 
+$entrada = "Lucas Emanuel, 2005, Programador";
+$saida = explode(", ", $entrada);
+
+var_dump($saida);
 
 ?>
