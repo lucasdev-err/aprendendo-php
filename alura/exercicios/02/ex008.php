4c80c27bb3c3b74a5aca6ba526c5693f293f51da
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-23 20:04:28 -0300

    :white_check_mark: Exercicio 02/008 concluido

--- alura/exercicios/02/ex008.php
+++ alura/exercicios/02/ex008.php
@@ -2,4 +2,12 @@
 
 // Escreva um programa em PHP que abra o arquivo teste.txt, acrescente a frase "PHP é incrível!" no final do arquivo e depois o feche.
 
+$caminhoArquivo = __DIR__ . '/teste.txt';
+
+$arquivo = fopen($caminhoArquivo, 'a');
+fwrite($arquivo, "\nPHP é incrível!");
+fclose($arquivo);
+
+echo file_get_contents($caminhoArquivo);
+
 ?>
