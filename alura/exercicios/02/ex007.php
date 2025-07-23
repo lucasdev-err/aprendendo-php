274cf2852b03b7fc2e99ef87dea54edd04383b84
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-23 19:46:38 -0300

    :white_check_mark: Exercicio 007 concluido

--- alura/exercicios/02/ex007.php
+++ alura/exercicios/02/ex007.php
@@ -2,7 +2,12 @@
 
 // Escreva um programa em PHP que abra um arquivo chamado teste.txt para ler a primeira linha e depois o fecha.
 
+$caminhoArquivo = __DIR__ . '/teste.txt';
 
+$arquivo = fopen($caminhoArquivo, 'r');
+$primeiraLinha = fgets($arquivo);
+fclose($arquivo);
 
+echo $primeiraLinha;
 
 ?>
