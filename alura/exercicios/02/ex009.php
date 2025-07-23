4b131f1f516cac5d43c4ca6eecc3880931f73255
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-07-23 20:05:05 -0300

    :white_check_mark: Exercicio 02/009 concluido

--- alura/exercicios/02/ex009.php
+++ alura/exercicios/02/ex009.php
@@ -1,5 +1,9 @@
 <?php 
 
 // Crie um programa que converta a string '{"nome":"Vinicius","anoNascimento":1997,"profissao":"Dev"}' em um objeto.
+$string = '{"nome":"Vinicius","anoNascimento":1997,"profissao":"Dev"}';
+$objeto = json_decode($string);
+
+var_dump($objeto);
 
 ?>
