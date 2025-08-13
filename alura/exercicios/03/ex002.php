38a78be77e99799809d7dee19c97d3a0d82c85ca
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-08-13 20:04:27 -0300

    :truck: Atividades propostas

---
+++ alura/exercicios/03/ex002.php
@@ -0,0 +1,5 @@
+<?php 
+
+// Crie uma enum em PHP com tipos de contas bancárias e implemente um método informando se a conta possui taxas. Contas correntes e de investimento possuem taxas, enquanto contas poupança e universitárias não;
+
+?>
---
+++ alura/exercicios/03/ex003.php
@@ -0,0 +1,6 @@
+<?php 
+
+// Crie uma classe que represente uma conta com as propriedades saldo, nome do titular e tipo. Use os tipos e formas de acesso adequadas.
+
+
+?>
