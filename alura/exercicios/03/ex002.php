e97d9e3a64275737d7bc91418a2101985c01023d
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-08-15 20:40:22 -0300

    :white_check_mark: Exercicio 03/002 concluido

--- alura/exercicios/03/ex002.php
+++ alura/exercicios/03/ex002.php
@@ -2,4 +2,19 @@
 
 // Crie uma enum em PHP com tipos de contas bancárias e implemente um método informando se a conta possui taxas. Contas correntes e de investimento possuem taxas, enquanto contas poupança e universitárias não;
 
+enum Contas {
+    case Corrente;
+    case Investimento;
+    case Poupanca;
+    case Universitaria;
+    
+    public function possuiTaxas(): bool
+    {
+        return match($this){
+            Contas::Corrente, Contas::Investimento => true,
+            Contas::Poupanca, Contas::Universitaria => false
+        };
+    }
+}
+
 ?>
