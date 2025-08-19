58e334cd0c4c6522cc616731a7c1a113b498c6c5
Lucas <126705351+lucasdev-err@users.noreply.github.com>
2025-08-19 20:37:30 -0300

    :sparkles: Utilizando heranca

--- alura/screenMatch-alura/index.php
+++ alura/screenMatch-alura/index.php
@@ -1,14 +1,17 @@
 <?php 
 
 require __DIR__ . "/src/models/Genero.php";
+require __DIR__ . "/src/models/Titulo.php";
 require __DIR__ . "/src/models/Filme.php";
+require __DIR__ . "/src/models/Serie.php";
 
 echo "Bem-vindo(a) ao ScreenMatch\n";
 
 $filme = new Filme(
     'Thor - Ragnarok',
     2021,
-    Genero::SuperHeroi
+    Genero::SuperHeroi,
+    180,
 );
 
 $filme->avalia(10);
@@ -20,6 +23,14 @@ var_dump($filme);
 
 echo $filme->media() . "\n";
 
-echo $filme->anoLancamento;
+echo $filme->anoLancamento . PHP_EOL;
+
+$serie = new Serie("Lost", 2007, Genero::Drama, 10, 20, 50);
+
+echo $serie->anoLancamento . PHP_EOL;
+
+$serie->avalia(10);
+
+echo $serie->media() . PHP_EOL;
 
 ?>
